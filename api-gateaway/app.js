807237588a3ffee50c7103bb889da8298649249c
daaffalbari <daffa.10120212@mahasiswa.unikom.ac.id>
2022-06-22 15:42:26 +0700

    First commit

--- api-gateaway/app.js
+++ api-gateaway/app.js
@@ -1,12 +1,18 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require('dotenv').config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const coursesRouter = require('./routes/courses');
+const mediaRouter = require('./routes/media');
+const ordersRouter = require('./routes/orders');
+const paymentsRouter = require('./routes/payments');
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,5 +22,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/courses', coursesRouter);
+app.use('/media', mediaRouter);
+app.use('/payments', paymentsRouter);
+app.use('/orders', ordersRouter);
 
 module.exports = app;
